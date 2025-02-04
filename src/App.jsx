629711cd8381e83f8bea4e8be325e2d0e7bb07ce
Ricardo Braga <ricardo.pbraga@hotmail.com>
2025-02-04 19:50:49 -0300

    Criando o componente CategoryItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import CategoryItem from './components/CategoryItem/CategoryItem';
+import './categories.styles.scss';
+
+
 const App = () => {
   const categories = [
-    { id: 1, title: 'Hats' },
-    { id: 2, title: 'Jackets' },
-    { id: 3, title: 'Sneakers' },
-    { id: 4, title: 'Womens' },
-    { id: 5, title: 'Mens' },
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({ id, title }) => (
-        <div className="category-container" key={id}>
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
